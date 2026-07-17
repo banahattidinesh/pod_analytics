878092853cc292001b4621b31ed1057b23d43e21
bdinesh <153442292+banahattidinesh@users.noreply.github.com>
2026-07-17 12:36:16 +0000

    Update daily sales pipeline DAG with data extraction and transformation tasks

--- dags/02_daily_sales_pipeline.py
+++ dags/02_daily_sales_pipeline.py
@@ -1,21 +1,37 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 
-# Define the DAG with a daily schedule
+# A standard Python function that will act as our extraction script
+def download_data():
+    print("SUCCESS: Downloading orders from Shopify API into AWS S3...")
+
 with DAG(
     dag_id="pod_daily_sales_automation",
     start_date=datetime(2026, 7, 15),
     schedule="@daily",
     catchup=False,
     tags=["pod_business", "production"],
+    
 ) as dag:
 
-    # Task 1: A placeholder for our future data extraction script
-    extract_orders = EmptyOperator(
-        task_id="extract_shopify_orders"
+    # Task 1: Replaced with a PythonOperator
+    extract_orders = PythonOperator(
+        task_id="extract_shopify_orders",
+        python_callable=download_data
     )
 
-    transform_data = EmptyOperator(
-        task_id= "dbt_run_sales_models"
+    # Task 2: Replaced with a BashOperator
+    transform_data = BashOperator(
+        task_id="dbt_run_sales_models",
+        bash_command="echo 'SUCCESS: Running dbt models in the warehouse...'"
     )
+
+    # Task 3: Replaced with a BashOperator
+    notify_team = BashOperator(
+        task_id="send_success_email",
+        bash_command="echo 'Pipeline complete! Emailing the team.'"
+    )
+
+    extract_orders >> transform_data >> notify_team
